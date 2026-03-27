6271f20ca664985135047f1c7d870021c7c66481
Sjur <sjurbarndon@proton.me>
2026-03-27 14:37:07 +0100

    testing debounce and other timings

--- config.h
+++ config.h
@@ -1,3 +1,10 @@
 /* Setting required for single layer combos*/
 #define COMBO_SHOULD_TRIGGER
 #define COMBO_MUST_TAP_PER_COMBO
+
+
+/* Trying to increse the debounce time. Default is 5.*/
+#define DEBOUNCE 20
+#define COMBO_TERM 50
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 150
